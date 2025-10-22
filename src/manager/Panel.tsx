0ee974624efbf21cd2ec145b7526cdf4f173ff86
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-10-22 20:33:37 +0200

    style: Lint code
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- src/manager/Panel.tsx
+++ src/manager/Panel.tsx
@@ -1,12 +1,17 @@
 import React, { Fragment, memo, useEffect, useMemo, useState } from 'react'
-import { Bar, AddonPanel, Button, ScrollArea } from 'storybook/internal/components'
+import {
+  Bar,
+  AddonPanel,
+  Button,
+  ScrollArea,
+} from 'storybook/internal/components'
 
 import { useGlobals, useParameter } from 'storybook/manager-api'
 import { DeleteIcon, SweepIcon } from '@storybook/icons'
 import { ObjectControl } from '@storybook/addon-docs/blocks'
 
 import { PARAM_ENCODING_KEY, PARAM_KEY, PARAM_PRESERVE_KEY } from '../constants'
-import { clearCookies, existingCookies, getExistingCookies, setCookies } from '../cookies'
+import { clearCookies, existingCookies, setCookies } from '../cookies'
 import type { Cookie } from '../types'
 import { styled } from 'storybook/theming'
 
@@ -18,11 +23,11 @@ const Wrapper = styled(ScrollArea)(() => ({
   height: '100%',
   margin: 0,
   padding: '10px 5px 20px',
-}));
+}))
 
 const StyledBar = styled(Bar)(({ theme }) => ({
   background: theme.background.app,
-}));
+}))
 
 const Actions = styled.div({
   display: 'flex',
@@ -31,7 +36,7 @@ const Actions = styled.div({
   gap: '6px',
   paddingInlineStart: '6px',
   paddingInlineEnd: '15px',
-});
+})
 
 export const PanelContent = () => {
   const parameterCookie = useParameter<Cookie | null>(PARAM_KEY, null)
@@ -39,14 +44,23 @@ export const PanelContent = () => {
   const preserveCookies = useParameter<boolean>(PARAM_PRESERVE_KEY, false)
 
   const defaultCookie = useMemo(() => {
-    return preserveCookies ? { ...existingCookies, ...parameterCookie } : parameterCookie
+    return preserveCookies
+      ? { ...existingCookies, ...parameterCookie }
+      : parameterCookie
   }, [parameterCookie, preserveCookies, existingCookies])
 
   const [value, setValue] = useState<Cookie>()
   const [globals, updateGlobals] = useGlobals()
 
   const updateCookieValue = (newValue: Cookie | null, forceClear = false) => {
-    console.log('clearing, forceClear:', forceClear, 'preserve:', preserveCookies, 'newValue:', newValue)
+    console.log(
+      'clearing, forceClear:',
+      forceClear,
+      'preserve:',
+      preserveCookies,
+      'newValue:',
+      newValue,
+    )
     setValue(newValue ?? {})
 
     if (forceClear || preserveCookies !== true) {
@@ -81,11 +95,21 @@ export const PanelContent = () => {
     <Fragment>
       <StyledBar border>
         <Actions>
-          <Button variant="ghost" padding="small" size="small" onClick={handleClear}>
+          <Button
+            variant="ghost"
+            padding="small"
+            size="small"
+            onClick={handleClear}
+          >
             <DeleteIcon />
             Clear
           </Button>
-          <Button variant="ghost" padding="small" size="small" onClick={handleReset}>
+          <Button
+            variant="ghost"
+            padding="small"
+            size="small"
+            onClick={handleReset}
+          >
             <SweepIcon />
             Reset
           </Button>
